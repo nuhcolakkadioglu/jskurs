95ce94d333b9f75b9f18788573a8f3159a5522a6
nuhcolakkadioglu <hex-tr@hotmail.com>
2021-08-23 13:12:31 +0300

    kurs silme işlemi bitti

--- appES5.js
+++ appES5.js
@@ -18,16 +18,24 @@ UI.prototype.addCourseToList = function (course) {
                 <td><img src="img/${course.image}" width="100" /></td>
                 <td>${course.title}</td>
                 <td>${course.instructor}</td>
-                <td><a href="#" class="btn btn-danger btn-sm">Düzenle</a></td>
+                <td><a href="#" class="btn btn-danger btn-sm delete">sil</a></td>
                 </tr>
                 `;
     list.innerHTML+=html;
 }
+//form kontrollerinin kayıtdan sonra temizlenmesi 
 UI.prototype.clearsControls = function () {
     const title = document.getElementById('title').value="";
     const instructor = document.getElementById('instructor').value="";
     const image = document.getElementById('image').value="";
 
+}
+//kurs silme
+
+UI.prototype.deleteCourse = function(element){
+    if(element.classList.contains('delete')){
+        element.parentElement.parentElement.remove();
+    }
 }
 
 document.getElementById('new-course').addEventListener('submit', function (e) {
@@ -53,3 +61,12 @@ document.getElementById('new-course').addEventListener('submit', function (e) {
 
     e.preventDefault();
 });
+
+document.getElementById("course-list").addEventListener('click',function(e){
+
+     const ui = new UI();
+     ui.deleteCourse(e.target);
+       
+
+    e.preventDefault();
+})
